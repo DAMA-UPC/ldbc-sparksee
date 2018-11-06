7586433df477a32e26f88f445b9d17798fdfa063
ArnauPrat <arnau.prat@gmail.com>
2018-11-06 17:17:30 +0100

    Fixed python scripts

--- scripts/shutdownServer.py
+++ scripts/shutdownServer.py
@@ -1,7 +1,7 @@
 import socket
 import sys
 
-print "Shutting down server"
+print("Shutting down server")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = sys.argv[1] 
 port = 9999
@@ -9,4 +9,4 @@ port = 9999
 s.connect((host,port))
 s.close ()
 
-print "Server shutdown"
+print("Server shutdown")
